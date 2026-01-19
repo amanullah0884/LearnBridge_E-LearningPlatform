0fc1f7ff903cd8c305c52914b562d84ca97a06c8
amanullah0884 <amanullah0884@gmail.com>
2026-01-19 14:01:32 +0600

    program

---
+++ LearnBridge_E-LearningPlatform/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LearnBridge_E_LearningPlatform.Models
+{
+    public class ApplicationUser: IdentityUser
+
+    {
+        // Additional properties can be added here as needed
+        public string? FullName { get; set; }
+    }
+}
--- LearnBridge_E-LearningPlatform/Program.cs
+++ LearnBridge_E-LearningPlatform/Program.cs
@@ -1,4 +1,5 @@
 using LearnBridge_E_LearningPlatform.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +8,30 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+var jwt = builder.Configuration.GetSection("Jwt");
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwt["Issuer"],
+            ValidAudience = jwt["Audience"],
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwt["Key"]))
+        };
+    });
 var app = builder.Build();
 
 // Custom Global Exception Middleware
 app.UseMiddleware<ExceptionMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHsts();
 app.UseHttpsRedirection();
